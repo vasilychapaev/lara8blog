3a96168319d215d6426bf73e02fa7e6559c7c916
vasilychapaev <vasilychapaev@gmail.com>
2021-10-07 16:35:05 +0200

    init steps

---
+++ database/migrations/2021_10_07_071103_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('description');
+            $table->text('content');
+            $table->integer('category_id');
+            $table->string('thumbnail');
+            $table->integer('views')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::any('adminer', '\Aranyasen\LaravelAdminer\AdminerAutologinController@index');
+
+
+require __DIR__.'/auth.php';
